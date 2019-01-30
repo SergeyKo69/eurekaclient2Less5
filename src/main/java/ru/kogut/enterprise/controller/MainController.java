a2fa42ce235a73c3722020bd064a8dc7da9cb95d
Kogut Sergey <koguts@yandex.ru>
2019-01-30 20:38:00 +0300

    Initial commit

---
+++ src/main/java/ru/kogut/enterprise/controller/MainController.java
@@ -0,0 +1,25 @@
+package ru.kogut.enterprise.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class MainController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/service/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
+    }
+}
